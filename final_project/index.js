c9eafc10ffa289ad38daca6a13dab18a73541c58
jaybarmeda4152 <121751261+jaybarmeda4152@users.noreply.github.com>
2024-03-23 23:17:45 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -4,7 +4,7 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
-const app = express();
+const app = express(); 
 
 app.use(express.json());
 
